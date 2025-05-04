4c43727dd29bef493a55352144e59f00e73a59b2
vijay kumar <vishaala999111@gmail.com>
2025-05-04 12:22:58 +0530

    commit

---
+++ backend/lib/redis.js
@@ -0,0 +1,6 @@
+import Redis from "ioredis";
+import dotenv from "dotenv";
+dotenv.config();
+const client = new Redis(process.env.UPSTASH_REDIS_URL);
+await client.set("foo", "bar");
+// client here is aremote or a connector or a tool to talk to the redis database
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -8,3 +8,4 @@ router.post("/logout", logout);
 export default router;
 // This code defines an Express router for handling authentication routes.
 //hello
+//UPSTASH_REDIS_URL "rediss://default:AUtnAAIjcDE4YjUzZWE4YTE4Y2U0NDQ5YjU4MjVlMjM5NWJjMjlmYnAxMA@close-imp-19303.upstash.io:6379"
