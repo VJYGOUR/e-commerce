29844e7e32ad9820d7b3a28b4e5ced07bf6a5962
vijay kumar <kumarvijayy036@gmail.com>
2025-05-20 22:02:38 +0530

    commit

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
+import { useUserStore } from "../stores/useUserStore";
 function SignUpPage() {
   const loading = false;
   const [formData, setFormData] = useState({
@@ -10,9 +11,10 @@ function SignUpPage() {
     password: "",
     confirmPassword: "",
   });
+  const { signup, user } = useUserStore();
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    signup(formData);
   };
   return (
     <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
