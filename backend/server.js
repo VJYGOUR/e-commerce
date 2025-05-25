d750e52b4d26a036e9329ff0a6ee39fe91638544
vijay kumar <kumarvijayy036@gmail.com>
2025-05-25 22:29:04 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,13 @@ import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
-import cors from "cors";
+
 import { connect } from "mongoose";
 import { connectDB } from "./lib/db.js";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 app.use(express.json()); // this middleware execute for every request and you write it only once thats it , no need to write again and again for every sebsequent request.
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Your Vite dev server
-    credentials: true,
-  })
-);
+
 app.use(cookieParser()); // this middleware is used to parse the cookies from the request
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
